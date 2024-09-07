cfb53671c59cc2426e5bfe2c27783987f4540733
siddhi2218 <siddhi2218@localhost>
2024-09-07 11:42:05 -0500

    1) Done with point.java file 2) Implemented necessary methods as mentioned in homework for point.java 3)Implemented necessary override method as mentioned in the homework file for point.java

--- src/edu/uwm/cs351/Point.java
+++ src/edu/uwm/cs351/Point.java
@@ -1,4 +1,4 @@
-package edu.uwm.cs351;
+	package edu.uwm.cs351;
 
 /**
  * A point in two-dimensional Euclidean space.
@@ -6,8 +6,67 @@ package edu.uwm.cs351;
  */
 public class Point {
 	private final double x, y;
+	
+	public Point(double x,double y){
+		
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double x() {
+		
+		return x;
+	}
+	
+	public double y() {
+		
+		return y;
+	}
 
+	public int intX() {
+		
+		return (int) Math.round(x);
+		
+	}
+	
+    public int intY() {
+		
+		return (int) Math.round(y);
+		
+	}
+    
+    public double distance(Point other) {
+    	double dx,dy;
+    	dx = this.x - other.x();
+    	dy = this.y - other.y();
+    	return Math.sqrt(dx*dx + dy*dy);
+    	
+    }
 	// TODO implement this class
-	// Add javadoc for everything public except @Override methods.
-
+	
+    @Override 
+    public String toString() {
+    	return "("+ x + ","+ y + ")";
+    	
+    }
+    
+    @Override 
+    public boolean equals(Object obj) {
+    	if(obj instanceof Point)
+    	{
+    		Point p = (Point) obj;
+    		return(p.x == x) && (p.y ==y);
+    	}
+    	else
+    		return false;
+    	
+    }
+    
+    @Override 
+    public int hashCode() {
+    	return (int) (7*x + 23 * y);
+    	
+    }
+    
+    
 }
