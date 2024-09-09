91c34d2dbc35ce924213befd803f149be2d85bc0
siddhi2218 <siddhi2218@siddhis-mbp.lan>
2024-09-09 09:40:30 -0500

    Added the javadoc comments to the final code

--- src/edu/uwm/cs351/Point.java
+++ src/edu/uwm/cs351/Point.java
@@ -1,72 +1,107 @@
-	package edu.uwm.cs351;
+package edu.uwm.cs351;
 
 /**
+ * @author siddhi2218
  * A point in two-dimensional Euclidean space.
  * This class is immutable.
  */
-public class Point {
-	private final double x, y;
+
+public class Point implements Cloneable{
+	private final double x;
+	final double y;
+	
+	/**
+	 * Generates a new point with specified cordinates.
+	 *
+	 * @param x the x co-ordinate of the point
+	 * @param y the y co-ordinate of the point
+	 */
 	
 	public Point(double x,double y){
-		
-		this.x = x;
+		this.x = x;       
 		this.y = y;
 	}
 	
-	public double x() {
-		
-		return x;
-	}
+	/**
+	 * Gets the x co-ordinate of the point.
+	 *
+	 * @return the x co-ordinate
+	 */
 	
-	public double y() {
-		
-		return y;
-	}
+	public double x() {return x;}
+	
+	/**
+	 * Gets the y co-ordinate of the point.
+	 *
+	 * @return the y co-ordinate
+	 */
+	
+	public double y() {return y;}
+	
+	/**
+	 * Gets the rounded x co-ordinate of the point.
+	 *
+	 * @return the rounded x co-ordinate
+	 */
 
-	public int intX() {
-		
-		return (int) Math.round(x);
-		
-	}
+	public int intX() {return (int) Math.round(x);}
 	
-    public int intY() {
-		
-		return (int) Math.round(y);
-		
-	}
+	/**
+	 * Gets the rounded y co-ordinate of the point.
+	 *
+	 * @return the rounded y co-ordinate
+	 */
+	
+    public int intY() {return (int) Math.round(y);}
+    
+    /**
+	 * Calculates the Euclidean distance from the current point to other point.
+	 * 
+	 * @param other the other point
+	 * @return the distance between the two points
+	 */
     
     public double distance(Point other) {
-    	double dx,dy;
-    	dx = this.x - other.x();
-    	dy = this.y - other.y();
-    	return Math.sqrt(dx*dx + dy*dy);
+    	double distx,disty;
+    	distx = this.x - other.x();
+    	disty = this.y - other.y();
+    	return Math.sqrt(distx*distx + disty*disty);
     	
     }
-	// TODO implement this class
-	
-    @Override 
-    public String toString() {
-    	return "("+ x + ","+ y + ")";
-    	
-    }
+    /**
+   	 * Compares the location of the point to another object for equality 
+   	 * 
+   	 * @param o an object with which this location is compared
+   	 * @return Return true if the other point’s coordinates are exactly the same
+   	 */
     
     @Override 
-    public boolean equals(Object obj) {
-    	if(obj instanceof Point)
+    public boolean equals(Object o) {
+    	if(o instanceof Point)
     	{
-    		Point p = (Point) obj;
-    		return(p.x == x) && (p.y ==y);
+    		Point p = (Point) o;
+    		return(p.x==x) && (p.y==y);
     	}
     	else
     		return false;
-    	
     }
+    
+    /**
+   	 * Gets the hashcode of the point with the formula : 7*x +23*y
+   	 * 
+   	 * @return the hashcode of the point
+   	 */
     
     @Override 
-    public int hashCode() {
-    	return (int) (7*x + 23 * y);
-    	
-    }
+    public int hashCode() {return (int) (7*x + 23*y);}
     
+    /**
+   	 * Gets the string representation of the point in a format (x,y)
+   	 * 
+   	 * @return the x and y co-ordinate of the points
+   	 */
     
+    @Override 
+    public String toString() {return "("+ x + ","+ y + ")";}
+     
 }
